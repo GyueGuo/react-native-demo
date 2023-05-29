b23efe7b25ab99032216103fda870856d1bc28b7
guojy01 <guojy01@rabyte.cn>
2023-05-29 18:31:51 +0800

    scan

--- src/modules/home/views/index/components/search/index.jsx
+++ src/modules/home/views/index/components/search/index.jsx
@@ -1,7 +1,9 @@
-import React, { useMemo } from "react";
-import { View } from "react-native";
-import { safeAreaHeight } from '../../../../../../components/navigator'
+import React, { useCallback, useMemo, useState } from "react";
+import { View, TouchableWithoutFeedback, Text } from "react-native";
+import SwiperFlatList from 'react-native-swiper-flatlist';
 
+import { safeAreaHeight } from '../../../../../../components/navigator'
+import ScanIcon from '../../../../../../assets/svg/scan.svg'
 import AdaptStyleSheet from "../../../../../../utils/adaptStyleSheet";
 
 const styles = AdaptStyleSheet.create({
@@ -19,21 +21,56 @@ const styles = AdaptStyleSheet.create({
   },
   search: {
     backgroundColor: '#fff',
-    borderRadius: 13,
-    height: 26,
+    borderRadius: 15,
+    height: 30,
     flex: 1,
+    overflow: 'hidden',
+  },
+  icon: {
+    marginLeft: 10,
+  },
+  swiperItem: {
+    paddingLeft: 10,
+    paddingRight: 10,
+    width: '100%',
+    lineHeight: 30,
+    height: 30,
   }
 })
 
 export default function () {
+  const [list, setList ] = useState([1,2,3]);
   const finalWrapStyles = useMemo(() => ({
     ...styles.wrap,
     paddingTop: styles.wrap.paddingLeft + safeAreaHeight
-  }), [])
+  }), []);
+  const handleScan = useCallback(() => {
+
+  }, []);
+  const handleSearch = useCallback(() => {
+
+  }, []);
+  const renderSwiperItem = useCallback((data) => {
+    console.log(data)
+    return (
+      <Text key={data} style={styles.swiperItem} numberOfLines={1}>{data}</Text>
+    )
+  }, []);
   return (
     <View style={finalWrapStyles}>
-      <View style={styles.search}></View>
-      <View style={styles.icon}></View>
+      <TouchableWithoutFeedback onPress={handleSearch}>
+        <View style={styles.search}>
+          <SwiperFlatList
+            vertical
+            autoplay
+            autoplayLoop
+            autoplayLoopKeepAnimation
+            data={list}
+            renderItem={renderSwiperItem}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <ScanIcon style={styles.icon} color="#fff" width={30} height={30} onClick={handleScan}/>
     </View>
   )
 }
